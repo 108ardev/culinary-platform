3cfef88b11aacfa068ec942627acb78f6d1cb4b9
Andrew Romanov <r0m4n0v4v@gmail.com>
2025-04-07 21:46:46 +0300

    changed db config

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Client } = require('pg');
 
 const client = new Client({
   user: 'myuser',           // имя пользователя PostgreSQL
-  host: 'localhost',        // адрес сервера базы данных
+  host: '109.73.205.129',        // адрес сервера базы данных
   database: 'culinarydb',   // имя базы данных
   password: 'mypassword',   // пароль пользователя
   port: 5432,               // порт PostgreSQL (по умолчанию 5432)
